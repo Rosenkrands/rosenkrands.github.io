23c6e21e39b7c02f5f13547d3ca3d45c719bc43d
Rosenkrands <KRosenkrands7@gmail.com>
2020-02-02 16:50:58 +0100

    Tweaked the script to work after reset

--- generator.ps1
+++ generator.ps1
@@ -1,6 +1,6 @@
-﻿cd 'C:\Users\krose\Desktop\GitHub Repos\rosenkrands.github.io'
+﻿cd 'C:\Users\Kasper\Desktop\github_repos\rosenkrands.github.io'
 
-$dir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
+#$dir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
 
 $html_basic = 
 @"
@@ -24,7 +24,14 @@ $html_basic =
 
 Set-Content -Path .\projects.html -Value $html_basic
 
-$blackboard_folders = get-childitem -Path "$dir\files\blackboard"
+$blackboard_folders = get-childitem -Path .\files\blackboard
+
+write-host "The following folders was found"
+write-host "-------------------------------"
+forEach($f in $blackboard_folders.Name) {
+$f
+}
+write-host "-------------------------------"
 
 Add-content -path .\projects.html -value "<a href = index.html>Homepage</a>"
 
@@ -35,19 +42,23 @@ ForEach ($f in $blackboard_folders.name) {
 ForEach ($f in $blackboard_folders.name) {
 $semester_heading = "<a id = ""$f""><h1>$f</h1></a>"
 Add-Content -Path .\projects.html -value $semester_heading
-$courses = Get-ChildItem -Path "$dir\files\blackboard\$f"
+$courses = Get-ChildItem -Path .\files\blackboard\$f
 
 ForEach ($c in $courses.name) {
 Add-content -path .\projects.html -value "<a href = ""#$c"" rel = ""next"">$c</a>"
 }
     ForEach ($c in $courses.name) {
+    write-host "-------------------------------"
+    write-host "Found the course $c from $f"
     $course_heading = "<a id = ""$c""><h2>$c</h2><p></a><a href = ""#top"">top</a></p>"
     Add-content -Path .\projects.html -value $course_heading
-    $pictures = Get-ChildItem -path "$dir\files\blackboard\$f\$c"    
+    $pictures = Get-ChildItem -path .\files\blackboard\$f\$c   
         ForEach ($p in $pictures.name) {
             $html_picture = "<img src = ""files\blackboard\$f\$c\$p"", width = ""500"">"
             Add-Content -Path .\projects.html -value $html_picture
         }
+    write-host "Pictures from ""$c"" have been added"
+    write-host "-------------------------------"
     }
 }
 
